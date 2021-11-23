caa36cc15d5918bc525c2cab78a8683b81083ec5
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2021-11-23 14:23:46 -0300

    changed Coffee to .JS

---
+++ app/assets/javascripts/channels/room.js
@@ -0,0 +1,36 @@
+App.room = App.cable.subscriptions.create("RoomChannel", {
+  connected: function() {
+
+  },
+
+  disconnected: function() {
+
+  },
+  received: function(data) {
+     if (data.content != null) {
+      $('#messages-table').append('<div class="message">' + '<div class="message-user">' 
+      + data.username + ":" + '</div>' + '<div class="message-content">' 
+      + data.content + '</div>' + '</div>');
+      scroll_bottom();
+    }
+  }
+});
+
+$(document).on('turbolinks:load', function() {
+  submit_message();
+  return scroll_bottom();
+});
+
+submit_message = function() {
+  $('#message_content').on('keydown', function(event) {
+    if (event.keyCode === 13 && !event.shiftKey) {
+      $('input').click();
+      event.target.value = "";
+      event.preventDefault();
+    }
+  });
+};
+
+scroll_bottom = function() {
+  $('#messages').scrollTop($('#messages')[0].scrolllHeight);
+};
